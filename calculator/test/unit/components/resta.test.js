8757799458c34f99fe06b2b82675068482bdd5a8
Leonardo Rivas <168871358+leonardorivas1100@users.noreply.github.com>
2025-05-27 13:28:45 -0500

    Modificaciones para verificacion de cambios en Vercel

--- calculator/test/unit/components/resta.test.js
+++ calculator/test/unit/components/resta.test.js
@@ -1,5 +1,5 @@
 const resta = require("../../../src/components/resta.js");
 
-test("5 - 3 = 2", () => {
+test("Esta prueba unitaria debe restar 5 - 3 y si el resultado da 2 la prueba unitaria es correcta", () => {
   expect(resta(5, 3)).toBe(2);
 });
